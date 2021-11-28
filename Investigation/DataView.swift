8e3f51ae5e3dca8bdfc44a969fcc286c91a0fd6b
Hans Guntersson <daniel.harlos@gmail.com>
2021-11-28 19:14:19 +0000

    Further work on JSON structures and implemented dynamic  lists through  screens

--- Investigation/DataView.swift
+++ Investigation/DataView.swift
@@ -30,6 +30,8 @@ struct CaseItem: Hashable, Identifiable, Codable {
     var description: String
     var itemtype: itemtypekey
     var visible: Bool
+    var new: Bool
+    var caseitemdetails: [CaseItemDetail]
 }
 
 struct CaseItemDetail: Hashable, Identifiable, Codable {
@@ -41,6 +43,27 @@ struct CaseItemDetail: Hashable, Identifiable, Codable {
     var new: Bool
 }
 
+
+// RESOURCES
+
+/*
+
+struct Resource: Identifiable, Codable {
+    let id: Int
+    var code: String
+    let title: String
+    let description: String
+    let subresources: [SubResource]
+}
+
+struct SubResource: Identifiable, Codable {
+    let id: Int
+    var code: String
+    let title: String
+    let description: String
+}
+ 
+ */
 
 // ------------------------------------ DATA ------------------------------------ //
 
@@ -55,7 +78,6 @@ let case2 = Case(id: 2, code: "3378", title: "Missing Teenager", description: "A
 
 let case3 = Case(id: 3, code: "5598", title: "Shallow grave", description: "Remains have been found in a shallow grave in the woods", caseitems: [])
 
-
 // CASE ITEMS
 let caseitem1 = CaseItem(
     id: 1,
@@ -63,43 +85,41 @@ let caseitem1 = CaseItem(
     title: "Theft site",
     description: "The site of the theft",
     itemtype: .Location,
-    visible: false)
+    visible: false,
+    new: true,
+    caseitemdetails: [caseitemdetail1, caseitemdetail2])
 let caseitem2 = CaseItem(
     id: 2,
     code: "8875",
     title: "Rucksack",
     description: "A rucksack found near the theft site",
     itemtype: .Evidence,
-    visible: false)
+    visible: false,
+    new: true,
+    caseitemdetails: [caseitemdetail7])
 let caseitem3 = CaseItem(
     id: 3, code: "7765",
     title: "Barry Edmondson",
     description: "A witness to the theft",
     itemtype: .Person,
-    visible: false)
+    visible: false,
+    new: true,
+    caseitemdetails: [caseitemdetail3, caseitemdetail4])
 let caseitem4 = CaseItem(
     id: 4, code: "2245",
     title: "Hasaf Abwali",
     description: "A nearby shopkeeper",
     itemtype: .Person,
-    visible: false)
+    visible: false,
+    new: true,
+    caseitemdetails: [caseitemdetail5, caseitemdetail6])
 
 
 // CASE ITEM DETAILS
-let caseitemdetail1 = CaseItemDetail(id: 1, code: "7789", title: "Theft site", description: "The site of the theft", visible: false, new: true)
-let caseitemdetail2 = CaseItemDetail(id: 2, code: "5533", title: "Theft site", description: "The site of the theft", visible: false, new: true)
-let caseitemdetail3 = CaseItemDetail(id: 3, code: "6675", title: "Theft site", description: "The site of the theft", visible: false, new: true)
-let caseitemdetail4 = CaseItemDetail(id: 4, code: "8897", title: "Theft site", description: "The site of the theft", visible: false, new: true)
-let caseitemdetail5 = CaseItemDetail(id: 5, code: "3377", title: "Theft site", description: "The site of the theft", visible: false, new: true)
-let caseitemdetail6 = CaseItemDetail(id: 6, code: "7879", title: "Theft site", description: "The site of the theft", visible: false, new: true)
-let caseitemdetail7 = CaseItemDetail(id: 7, code: "8789", title: "Theft site", description: "The site of the theft", visible: false, new: true)
-
-
-
-// ------------------------------------ VIEW ------------------------------------ //
-
-struct DataView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+let caseitemdetail1 = CaseItemDetail(id: 1, code: "7789", title: "Skid marks", description: "Marks in  the road from tyres", visible: false, new: true)
+let caseitemdetail2 = CaseItemDetail(id: 2, code: "5533", title: "Kerb damage", description: "Damage to the kerb from impact", visible: false, new: true)
+let caseitemdetail3 = CaseItemDetail(id: 3, code: "6675", title: "Age", description: "42 years of age", visible: false, new: true)
+let caseitemdetail4 = CaseItemDetail(id: 4, code: "8897", title: "Profession", description: "Investment banker", visible: false, new: true)
+let caseitemdetail5 = CaseItemDetail(id: 5, code: "3377", title: "Age", description: "65 years of  age", visible: false, new: true)
+let caseitemdetail6 = CaseItemDetail(id: 6, code: "7879", title: "Profession", description: "Local shopkeeper", visible: false, new: true)
+let caseitemdetail7 = CaseItemDetail(id: 7, code: "8789", title: "Bag tag", description: "Tag with manufacturer ID", visible: false, new: true)
--- Investigation/TabDetailView.swift
+++ Investigation/TabDetailView.swift
@@ -7,22 +7,12 @@
 
 import SwiftUI
 
-struct ListItem: Identifiable {
-    let id: Int
-    let name: String
-    let description: String
-    var visible: Bool
-}
-
-let TestItem = ListItem(id: 1, name: "Test Item", description: "Test Description", visible: true)
-let TestItem2 = ListItem(id: 2, name: "Test Item 2", description: "Test Description 3", visible: false)
-let TestItem3 = ListItem(id: 3, name: "Test Item 3", description: "Test Description 3", visible: false)
-
 struct TabDetailView: View {
     let CaseItem: CaseItem
     
     var body: some View {
-        let items: [ListItem] = [TestItem, TestItem2, TestItem3]
+        
+        let items = CaseItem.caseitemdetails
         let ItemSelection = CaseItem.itemtype
         let ItemString = "\(ItemSelection): \(CaseItem.title)"
         
@@ -37,8 +27,8 @@ struct TabDetailView: View {
                     .padding(.horizontal, 10)
                     .padding(.vertical, 5)
                 List(items) { row in
-                    HStack {
-                        Text(row.name)
+                    VStack {
+                        Text(row.title)
                             .fontWeight(.bold)
                         Text(row.description)
                     }.padding(3)
@@ -78,6 +68,6 @@ func ButtonTextSwitcher(ItemInput: itemtypekey) -> String {
 
 struct TabDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        TabDetailView(CaseItem: CaseItem(id: 1, code: "4489", title: "Theft site", description: "The site of the theft", itemtype: .Location, visible: false))
+        TabDetailView(CaseItem: CaseItem(id: 1, code: "4489", title: "Theft site", description: "The site of the theft", itemtype: .Location, visible: false, new: true, caseitemdetails: []))
     }
 }
